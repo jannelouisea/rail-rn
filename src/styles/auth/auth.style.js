d265fec1c7fc4eddb9877df470814435f5f9f473
Janne Louise Ave <janne.louise.a@gmail.com>
2020-09-01 15:42:54 -0400

    Adding common auth style sheet

---
+++ src/styles/auth/auth.style.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native'
+import Theme from '../theme.style'
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 20,
+  },
+  headerText: {
+    fontSize: 21,
+    color: Theme.RAIL_GREEN,
+    marginBottom: 15,
+  },
+})
